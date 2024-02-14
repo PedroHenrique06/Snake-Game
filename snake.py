015eec00dc66562547ac1cf0a3595611c806ce12
Pedro Henrique <pedrosales2806@gmail.com>
2024-02-14 20:48:54 -0300

    Divisão das classes em pastas e organização de código.

---
+++ snake.py
@@ -0,0 +1,99 @@
+import pygame
+from cube import Cube
+
+ 
+# Classe que representa a "Cobra" do jogo        
+class Snake(object):
+    body = []
+    turns = {}
+    
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = Cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+        
+    def move(self):
+        
+        for event in pygame.event.get():
+            # Caso a aba seja fechada o programa encerra sem falhas
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+            keys = pygame.key.get_pressed()
+            
+            # Capta as direções através do teclado
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                    
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                    
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            
+            # Caso a cobra chegue a borda ela é transportada para a borda contraria
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows-1)
+                else: c.move(c.dirnx, c.dirny)
+    
+    # Reinicia a cobra para o menor tamanho possível
+    def reset(self, pos):
+        self.head = Cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
+        
+    # Aumenta o tamanho da cobra
+    def addCube(self):
+        tail = self.body[-1]
+        dx = tail.dirnx
+        dy = tail.dirny
+        
+        # Verifica a direção da cobra
+        if dx == 1 and dy == 0:
+            self.body.append(Cube((tail.pos[0]-1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(Cube((tail.pos[0]+1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(Cube((tail.pos[0], tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(Cube((tail.pos[0], tail.pos[1]+1)))
+            
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+    
+    # Desenha a cobra
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)  
